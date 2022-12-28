6458972329071712e3be338774148adc9b254388
OriiaC <edison_agudelo@hotmail.com>
2022-12-28 00:01:12 -0500

    ajuste ejercicio 1

--- Java/OpenBootCamp/src/Main.java
+++ Java/OpenBootCamp/src/Main.java
@@ -2,9 +2,8 @@ public class Main {
     public static void main(String[] args) {
         suma(1, 2, 3);
     }
-    public static int suma(int Num1, int Num2, int Num3) {
+    public static void suma(int Num1, int Num2, int Num3) {
         int Total = Num1 + Num2 + Num3;
         System.out.println(Total);
-        return Total;
     }
 }
