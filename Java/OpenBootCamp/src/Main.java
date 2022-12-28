35f51f531f220efc8f4a26e44edd6470466ffd3c
OriiaC <edison_agudelo@hotmail.com>
2022-12-28 00:21:08 -0500

    se agrega segunda parte del ejercicio pero genera error

--- Java/OpenBootCamp/src/Main.java
+++ Java/OpenBootCamp/src/Main.java
@@ -1,9 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         suma(1, 2, 3);
+        Coche miCoche = new Coche(); /*Me genera error aca, no entiendo por que*/
+        miCoche.ponerPuerta();
     }
+
     public static void suma(int Num1, int Num2, int Num3) {
         int Total = Num1 + Num2 + Num3;
         System.out.println(Total);
+    }
+class Coche {
+    public int puertas = 5;
+
+    public void ponerPuerta(){
+        this.puertas++;
+        System.out.println(puertas);
+    }
     }
 }
