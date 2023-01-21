19b37c8eff7822601216936dcd9a4d383399755f
OriiaC <edison_agudelo@hotmail.com>
2023-01-21 17:28:37 -0500

    se agregan los ejercicios del modulo 5. Bucles II

---
+++ C#/5. Bucles II/Ejercicio1 - If/Ejercicio1 - If/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Pida datos a un cliente: Nombre, email, cupón
+Determine si un cliente tiene un cupon descuento
+Muestre un precio rebajado en función del descuento
+Muestre el precio de un producto sin descuento si no hay cupón
+Nota: puedes poner un precio fijo de un producto o uno variable.*/
+
+
+int valorProducto = 100;
+int dctoCupon = 30;
+Console.Write("Ingrese su nombre: ");
+string? name = Convert.ToString(Console.ReadLine());
+
+Console.Write("Ingrese su email: ");
+string? email = Convert.ToString(Console.ReadLine());
+
+Console.Write("Tiene cupon? True/False: ");
+bool? cupon = Convert.ToBoolean(Console.ReadLine());
+
+if (cupon == true)
+{
+    Console.WriteLine("Sr/Sra " + name + " usted tiene un cupon de descuento del " + dctoCupon + "%, el total a pagar es de $" + ((100 - (dctoCupon * valorProducto) / 100)));
+}
+else
+{
+    Console.WriteLine("Sr/Sra " + name + " usted no tiene cupon de descuento, el total a pagar es de $" + valorProducto);
+}
---
+++ C#/5. Bucles II/Ejercicio2 - Switch/Ejercicio2 - Switch/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Haz una lista de lenguajes de programación, por ejemplo: C#, Java, C++.
+ * Presenta la lista en consola y pide que el usuario seleccione el lenguaje mediante 1, 2, 3 o a, b, c. Presenta el resultado en consola.
+Nota: puedes añadir al resultado el “Hola, mundo” para el caso de C#.*/
+
+Console.WriteLine("1. C#\n2. Java\n3. C++\n4. Python\n5. Go");
+Console.Write("Seleccione el lenguaje de programacion: ");
+int opt = Convert.ToInt16(Console.ReadLine());
+switch (opt)
+{
+    case 1:
+        Console.WriteLine("usted selecciono C#.");
+        break;
+    case 2:
+        Console.WriteLine("usted selecciono Java.");
+        break;
+    case 3:
+        Console.WriteLine("usted selecciono C++.");
+        break;
+    case 4:
+        Console.WriteLine("usted selecciono Python.");
+        break;
+    case 5:
+        Console.WriteLine("usted selecciono Go.");
+        break;
+    default: Console.WriteLine("La eleccion es invalida");
+        break;
+}
