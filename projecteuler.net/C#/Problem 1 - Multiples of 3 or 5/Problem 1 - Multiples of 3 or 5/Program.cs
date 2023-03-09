0f508c5af0758f423226a87b8aa26edcd2fa814f
OriiaC <edison_agudelo@hotmail.com>
2023-03-09 17:20:48 -0500

    se agregan todos los cambios

---
+++ projecteuler.net/C#/Problem 1 - Multiples of 3 or 5/Problem 1 - Multiples of 3 or 5/Program.cs
@@ -0,0 +1,17 @@
+﻿int total = 0;
+
+    for (int i = 1; i < 1000; i++)
+    {
+        int resultado3 = i % 3;
+        int resultado5 = i % 5;
+    if (resultado3 == 0)
+    {
+        total = total + i;
+    }
+    else if (resultado5 == 0)
+    {
+        total = total + i;
+    }
+    Console.WriteLine(total);
+}
+
---
+++ projecteuler.net/C#/Problem 2 - Even Fibonacci numbers/Problem 2 - Even Fibonacci numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿int num1 = 1;
+int num2 = 2;
+Console.WriteLine(num1);
+Console.WriteLine(num2);
+int siguiente = num1 + num2;
+Console.WriteLine(siguiente);
+ for (int i = 0; i < 10; i++)
+{
+    num1 = num2;
+    num2 = siguiente;
+    Console.WriteLine(siguiente);
+}
